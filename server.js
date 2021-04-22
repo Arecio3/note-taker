f5e029bfc5b19133b0ed3e3eda89f343962a09d1
Arecio Canton <cuba289@gmail.com>
2021-04-22 15:18:47 -0400

    finished api routes, and added uuid

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+
 // Sets up the Express App
 
 const app = express();
@@ -9,11 +10,15 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+// app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
 
 app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
